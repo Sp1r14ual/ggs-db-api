444ace6e26c2701c6901ae361d90d47d811c4c32
Sergey Panasenko <73416809+Sp1r14ual@users.noreply.github.com>
2024-05-23 23:15:31 +0700

    Refactored validate_data.py

--- validate_data.py
+++ validate_data.py
@@ -1,12 +1,4 @@
-def validate_person_add_data(data):
-    required_fields = ('family_name', 'name')
-
-    nullable_fields = ('birthdate', 'phone_number', 'patronimic_name',
-                       'pasport_serial', 'pasport_number', 'pasport_date', 'pasport_place',
-                       'dep_code', 'reg_adress', 'reg_region', 'reg_raion', 'reg_city',
-                       'reg_street', 'reg_house', 'postal_index', 'inn', 'ogrn', 'snils',
-                       'email')
-
+def proceed_validation(data, required_fields, nullable_fields=tuple()):
     for field in required_fields:
         if field not in data:
             return False, f"Missing required field: {field}"
@@ -16,6 +8,18 @@ def validate_person_add_data(data):
             return False, f'Unknown field is passed: {field}'
 
     return True, "OK"
+
+
+def validate_person_add_data(data):
+    required_fields = ('family_name', 'name')
+
+    nullable_fields = ('birthdate', 'phone_number', 'patronimic_name',
+                       'pasport_serial', 'pasport_number', 'pasport_date', 'pasport_place',
+                       'dep_code', 'reg_adress', 'reg_region', 'reg_raion', 'reg_city',
+                       'reg_street', 'reg_house', 'postal_index', 'inn', 'ogrn', 'snils',
+                       'email')
+
+    return proceed_validation(data, required_fields, nullable_fields)
 
 
 def validate_person_edit_data(data):
@@ -27,25 +31,13 @@ def validate_person_edit_data(data):
                        'reg_street', 'reg_house', 'postal_index', 'inn', 'ogrn', 'snils',
                        'email')
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    for field in data:
-        if (field not in required_fields) and (field not in nullable_fields):
-            return False, f'Unknown field is passed: {field}'
-
-    return True, "OK"
+    return proceed_validation(data, required_fields, nullable_fields)
 
 
 def validate_person_delete_data(data):
     required_fields = ('id',)
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    return True, "OK"
+    return proceed_validation(data, required_fields)
 
 
 def validate_organization_add_data(data):
@@ -56,15 +48,7 @@ def validate_organization_add_data(data):
     nullable_fields = ('adress_jur', 'zip_code_jur', 'adress_fact',
                        'zip_code_fact', 'remark', 'inn', 'bik',  'korr_acc', 'calc_acc', 'bank', 'ogrn')
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    for field in data:
-        if (field not in required_fields) and (field not in nullable_fields):
-            return False, f'Unknown field is passed: {field}'
-
-    return True, "OK"
+    return proceed_validation(data, required_fields, nullable_fields)
 
 
 def validate_organization_edit_data(data):
@@ -77,25 +61,13 @@ def validate_organization_edit_data(data):
                        'is_to_gvd_gnd', 'from_1c', 'to_rg', 'to_ggs', 'to_gss',
                        'to_ggsi', 'to_ggss', 'to_rgs')
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    for field in data:
-        if (field not in required_fields) and (field not in nullable_fields):
-            return False, f'Unknown field is passed: {field}'
-
-    return True, "OK"
+    return proceed_validation(data, required_fields, nullable_fields)
 
 
 def validate_organization_delete_data(data):
     required_fields = ('id',)
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    return True, "OK"
+    return proceed_validation(data, required_fields)
 
 
 def validate_house_add_data(data):
@@ -103,15 +75,7 @@ def validate_house_add_data(data):
 
     nullable_fields = ('cadastr_number',)
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    for field in data:
-        if (field not in required_fields) and (field not in nullable_fields):
-            return False, f'Unknown field is passed: {field}'
-
-    return True, "OK"
+    return proceed_validation(data, required_fields, nullable_fields)
 
 
 def validate_house_edit_data(data):
@@ -119,25 +83,13 @@ def validate_house_edit_data(data):
 
     nullable_fields = ('cadastr_number',)
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    for field in data:
-        if (field not in required_fields) and (field not in nullable_fields):
-            return False, f'Unknown field is passed: {field}'
-
-    return True, "OK"
+    return proceed_validation(data, required_fields, nullable_fields)
 
 
 def validate_house_delete_data(data):
     required_fields = ('id',)
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    return True, "OK"
+    return proceed_validation(data, required_fields)
 
 
 def validate_house_equip_add_data(data):
@@ -146,15 +98,7 @@ def validate_house_equip_add_data(data):
     nullable_fields = ('id_abonent', 'id_house',
                        'id_type_house_equip', 'year_produce', 'remark')
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    for field in data:
-        if (field not in required_fields) and (field not in nullable_fields):
-            return False, f'Unknown field is passed: {field}'
-
-    return True, "OK"
+    return proceed_validation(data, required_fields, nullable_fields)
 
 
 def validate_house_equip_edit_data(data):
@@ -163,22 +107,10 @@ def validate_house_equip_edit_data(data):
     nullable_fields = ('id_abonent', 'id_house',
                        'id_type_house_equip', 'year_produce', 'remark')
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    for field in data:
-        if (field not in required_fields) and (field not in nullable_fields):
-            return False, f'Unknown field is passed: {field}'
-
-    return True, "OK"
+    return proceed_validation(data, required_fields, nullable_fields)
 
 
 def validate_house_equip_delete_data(data):
     required_fields = ('id',)
 
-    for field in required_fields:
-        if field not in data:
-            return False, f"Missing required field: {field}"
-
-    return True, "OK"
+    return proceed_validation(data, required_fields)
